971fdcd983f3b68201b7e274b03cdaa286b0677a
jahircodes <jahircodes@gmail.com>
2026-03-14 22:42:26 +0530

    feat: add protected endpoint with token authentication

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const { authController } = require("../controllers");
 const { login: loginSchema } = require("../schemas/authSchema");
 const validate = require("../middlewares/validator");
+const authenticateToken = require("../middlewares/authenticateToken");
 
 router.post("/login", validate(loginSchema, "body"), authController.login);
 router.post("/refresh", authController.refreshToken);
 router.post("/logout", authController.logout);
+
+// Protected test endpoint
+router.get("/protected", authenticateToken, (req, res) => {
+  res.json({
+    success: true,
+    message: "You have accessed a protected route!",
+    user: req.user,
+  });
+});
 
 module.exports = router;
